859cb45000c64e042e174d4f6c0a04a66547fb7c
YutKigo <ykigoshi0829@gmail.com>
2024-10-31 02:29:05 +0900

    css color designated

--- src/components/RoomItem.js
+++ src/components/RoomItem.js
@@ -8,11 +8,16 @@ export const RoomItem = ({ roomsList, roomNumber, changeRoomStatus }) => {
         switch (status) {
             case "空室":
                 return "room-status-empty";
+            case "清掃指示":
+                return "room-status-requiredcleaning";
             case "清掃中":
                 return "room-status-cleaning";
+            case "点検中":
+              return "room-status-inspecting";
             case "清掃済":
-                return "room-status-cleaned";
-            
+              return "room-status-cleaned";
+            case "D/D":
+              return "room-status-dd";
             default:
                 return "";
     }
@@ -34,10 +39,15 @@ export const RoomItem = ({ roomsList, roomNumber, changeRoomStatus }) => {
                 
                 <select className='status-change' onChange={handleChangeRoomStatus}>
                   <option value="空室">空室</option>
+                  <option value="清掃指示">清掃指示</option>
                   <option value="清掃中">清掃中</option>
+                  <option value="点検中">点検中</option>
                   <option value="清掃済">清掃済</option>
+                  <option value="D/D">D/D</option>
+                  
+
                 </select>
-                <button >▶️</button>
+                {/*<button >▶️</button>*/}
               </p>
             ))}
           </>
